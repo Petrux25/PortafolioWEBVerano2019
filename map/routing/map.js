10457b644db4ab488910eb3afd309d397c1dd8fe
Diego Velásquez <diesveca@hotmail.com>
2020-01-12 20:15:31 -0600

    Mapas

---
+++ map/routing/map.js
@@ -0,0 +1,15 @@
+import waypoints from "./waypoins.js"
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZHVndWkiLCJhIjoiY2s1OGQyYWF2MGRoZjNsa2VpaDh5d3lzMyJ9.IkV7ks5D76hh89Wh0WncWg', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoiZHVndWkiLCJhIjoiY2s1OGQyYWF2MGRoZjNsa2VpaDh5d3lzMyJ9.IkV7ks5D76hh89Wh0WncWg'
+}).addTo(mymap);
+let w =  []
+waypoints.forEach(element => {
+  w.push(L.latLng(element.lat, element.lng))
+});
+L.Routing.control({
+    waypoints: w
+  }).addTo(mymap);
---
+++ map/routing/waypoins.js
@@ -0,0 +1,7 @@
+export default
+[ 
+        {"lat": "9.8495384", "lng": "-83.6101189", "name": "La Suiza"},
+        {"lat": "9.7901740", "lng": "-83.8508897", "name": "Orosi"},
+        {"lat": "9.84155", "lng": "-83.87817", "name": "Paraiso"},
+        {"lat": "9.8630277", "lng": "-83.9273618", "name": "Cartago"}
+    ]
